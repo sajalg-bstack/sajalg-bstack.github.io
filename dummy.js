d5f5cc0f39677bfd34e34423a86d873810698c5a
Sajal Garg <sajal.g@browserstack.com>
2026-04-10 10:42:53 +0530

    Added JS dummy.js

---
+++ dummy.js
@@ -0,0 +1,11 @@
+// A large dummy file to demonstrate compression
+console.log("Initializing the super-compressed application...");
+const data = Array(1000).fill("This is a repetitive string to show how well compression works.").join("\n");
+console.log("Data loaded.");
+
+// Change background color to red
+if (typeof document !== 'undefined') {
+    document.body.style.backgroundColor = 'red';
+    console.log("Background color changed to red");
+}
+
